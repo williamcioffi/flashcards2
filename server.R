e2788b93c4f5ee52d3fd54ff9675e1e2bc7cc30c
williamcioffi <william.cioffi@gmail.com>
2024-04-05 00:07:46 -0500

    fix global variables for multiple users and minor updates
    multiple users on shinyapps.io for instance would have their actions impact other users through the changing of the global variables (i did not know how shiny and global variables worked apparently!) but now that is fixed.
    
    minor updates:
    - add selection filters so you can look at just items of one criteria
    - z might have been common in the labels in the search so i changed it to ; for shuffling

--- server.R
+++ server.R
@@ -1,55 +1,107 @@
 library(shiny)
 
-# load files
-key <- read.table("imagekey.csv", header = TRUE, sep = ',', stringsAsFactors = FALSE)
-imagepaths <- file.path("images", key$filename)
-nimages <- nrow(key)
+# load files and make initial calculations
+key_orig <- read.table("imagekey.csv", header = TRUE, sep = ',', stringsAsFactors = FALSE)
+imagepaths <- file.path("images", key_orig$filename)
+nimages <- nrow(key_orig)
 curimage <- 1
 oo <- 1:nimages
 revealed <- FALSE
 
+ids_list <- sort(unlist(strsplit(unique(c(key_orig$info1, key_orig$info2, key_orig$info3, key_orig$info4, key_orig$info5)), "; ")))
+
 server <- function(input, output, session) {
+  # set up reactive values
+  values <- reactiveValues(
+    key_orig = key_orig,
+    key = key_orig,
+    imagepaths = imagepaths,
+    nimages = nimages,
+    curimage = curimage,
+    oo = 1:nimages,
+    ids_list = ids_list,
+    revealed = revealed
+  )
+  
+  observeEvent(input$info2, {
+    applyfilter()
+  })
+  
+  observeEvent(input$info3, {
+    applyfilter()
+  })
+  
+  observeEvent(input$info4, {
+    applyfilter()
+  })
+  
+  observeEvent(input$info5, {
+    applyfilter()
+  })
+  
+  
+  applyfilter <- function() {
+    filt <- rep(FALSE, nrow(values$key_orig))
+    
+    if(input$info2) filt <- filt | values$key_orig$info2 != ""
+    if(input$info3) filt <- filt | values$key_orig$info3 != ""
+    if(input$info4) filt <- filt | values$key_orig$info4 != ""
+    if(input$info5) filt <- filt | values$key_orig$info5 != ""
+    
+    values$key <- values$key_orig[filt, ]
+    values$imagepaths <- file.path("images", values$key$filename)
+    values$nimages <- nrow(values$key)
+    values$curimage <- 1
+    values$oo <- 1:values$nimages
+    values$revealed <- FALSE
+    values$ids_list <- sort(unlist(strsplit(unique(c(key_orig$info1, key_orig$info2, key_orig$info3, key_orig$info4, key_orig$info5)), "; ")))
+    
+    updateSelectInput(session, "id_select",
+      choices = values$ids_list,
+      selected = NULL
+    )
+  }
+  
   output$search <- renderUI(selectInput(
     "id_select",
-    "search id",
-    choices = key$label,
+    "search id/name",
+    choices = values$ids_list,
     multiple = TRUE
   ))
   
   observeEvent(input$id_select, {
     if(!is.null(input$id_select)) {
-      oo <<- grep(paste(input$id_select, collapse = "|"), key$label)
-      curimage <<- 1
-      revealed <<- FALSE
+      values$oo <- grep(paste(input$id_select, collapse = "|"), values$key$label)
     } else {
-      oo <<- 1:nimages
-      curimage <<- 1
-      revealed <<- FALSE
+      values$oo <- 1:values$nimages
     }
+    
+    values$curimage <- 1
+    values$revealed <- FALSE
   }, ignoreNULL = FALSE)
   
   reveal <- function() {
-    revealed <<- TRUE
+    values$revealed <- TRUE
   }
   
   prv <- function() {
-    revealed <<- FALSE
-    curimage <<- curimage - 1
-    if(curimage == 0) curimage <<- length(oo)
+    values$revealed <- FALSE
+    values$curimage <- values$curimage - 1
+    if(values$curimage == 0) values$curimage <- length(values$oo)
   }
   
   nxt <- function() {
-    revealed <<- FALSE
-    curimage <<- curimage + 1
-    if(curimage > length(oo)) curimage <<- 1
+    values$revealed <- FALSE
+    values$curimage <- values$curimage + 1
+    if(values$curimage > length(values$oo)) values$curimage <- 1
   }
   
   shuffle <- function() {
-    revealed <<- FALSE
-    oo <<- sample(1:nimages, nimages)
-    curimage <<- 1
+    values$revealed <- FALSE
+    values$oo <- sample(1:values$nimages, values$nimages)
+    values$curimage <- 1
     updateSelectInput(session, "id_select",
-      choices = ids_list,
+      choices = values$ids_list,
       selected = NULL
     )
   }
@@ -70,7 +122,7 @@ server <- function(input, output, session) {
     shuffle()
   })
   
-  observe({
+  observeEvent(input$keys, {
     curkey <- as.character(input$keys)
 
     if(length(curkey) == 1) {
@@ -78,18 +130,22 @@ server <- function(input, output, session) {
       "44" = prv(),
       "46" = nxt(),
       "47" = reveal(),
-      "122"  = shuffle()
+      "59"  = shuffle()
     )}
   })
   
   output$picture <- bindEvent({
     renderText({
-      out <- paste0("<p>", curimage, "/", length(oo), "</p>")
-      out <- paste0(out, "<br><img src=\"", imagepaths[oo[curimage]], "\" width = 100%>")
-      if(revealed) {
-        out <- paste0(out, "<br><p>", key$label[oo[curimage]], "</p>")
+      out <- "nothing selected"
+
+      if(values$nimages > 0) {
+        out <- paste0("<p>", values$curimage, "/", length(values$oo), "</p>")
+        out <- paste0(out, "<br><img src=\"", values$imagepaths[values$oo[values$curimage]], "\" width = 100%>")
+        if(values$revealed) {
+          out <- paste0(out, "<br><p>", values$key$label[values$oo[values$curimage]], "</p>")
+        }
       }
       
       out
-    })}, input$nxt, input$shuffle, input$prv, input$reveal, input$keys, input$id_select) 
+    })}, input$nxt, input$shuffle, input$prv, input$reveal, input$keys, input$id_select, input$info2, input$info3, input$info4, input$info5) 
 }
--- ui.r
+++ ui.r
@@ -11,12 +11,16 @@ ui <- fluidPage(
         actionButton("nxt", "next", style = "height: 30px; width: 70px; margin: 5px"),
         actionButton("prv", "prev", style = "height: 30px; width: 70px; margin: 5px"),
         actionButton("shuffle", "shuffle", style = "height: 30px; width: 70px; margin: 5px"),
-        uiOutput("search")
+        uiOutput("search"),
+        checkboxInput("info2", "crit. 1", value = TRUE),
+        checkboxInput("info3", "crit. 2", value = TRUE),
+        checkboxInput("info4", "crit. 3", value = TRUE),
+        checkboxInput("info5", "crit. 4", value = TRUE)
     ),
     width = 2),
     mainPanel(
-      HTML("<p>. = next<br>, = previous<br>/ = reveal<br>z = shuffle</p>"),
-      htmlOutput("picture"),
+      HTML("<p>. = next<br>, = previous<br>/ = reveal<br>; = shuffle</p>"),
+      htmlOutput("picture", style = "width: 50%"),
       tags$script('
         $(document).on("keypress", function (e) {
           Shiny.setInputValue("keys", e.which, {priority: "event"});
